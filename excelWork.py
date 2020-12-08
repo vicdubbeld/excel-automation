20631c05843feab559cf313651cf67bcd9b26233
Victor <victor@Hans-MacBook-Air.local>
2020-12-08 15:27:55 -0800

    Blocks for each resort. Next is to delete duplicate emails

--- excelWork.py
+++ excelWork.py
@@ -11,66 +11,194 @@ print('')
 print('-------------------------Converting HashString column to MD5 Hash & HexString column to ascii hex---------------------------------')
 print('')
 
-
 now = datetime.now()
-today_date = now.strftime("%m-%d-%Y")
+today_date = now.strftime("%M-%m-%d-%Y")
+print("Good Morning! Today is: ", today_date)
 
-print("The date is: ", today_date)
+
+
+
 
 
 # Capital Block
-path = os.getcwd()
-print(path)
-# os.chdir(os.path.dirname('/Users/victor/PYTHON-SURVEY-DOWNLOADS/'))
-directory = '/Users/victor/excel-auto/excel-automation/PYTHON-SURVEY-DOWNLOADS/'
+with open('/Users/victor/PYTHON-SURVEY-DOWNLOADS/Capital_Survey_Invites_-_New_Survey.csv') as csvfile:
+    # with open('/Users/victor/12-06-20CAPITAL.csv', "r+") as output:
+    with open('/Users/victor/Dropbox (Perspective Group)/Victor/SURVEYS/DAILY WORK/'+ today_date +'CAPITALpython.csv', "a") as output:
+        reader = csv.DictReader(csvfile, quotechar='"', delimiter=",", quoting=csv.QUOTE_ALL, skipinitialspace=True)
+        writer = csv.DictWriter(output, reader.fieldnames)
+        for i, r in enumerate(reader):
+            # writing csv headers
+            if i == 0:
+                output.write(','.join(r) + '\n')
 
-# for root,dirs,files in os.walk(path): 
-for file in os.listdir(directory):
-    # for file in files:
-        # print("verified files are only csv...")
-        if file.endswith('.csv'):
-            print(os.path.join(directory, file))
-            # f=open(file, 'r')
-            # with open('/Users/victor/PYTHON-SURVEY-DOWNLOADS/Capital_Survey_Invites_-_New_Survey.csv') as csvfile4:
-            with open(os.path.join(directory, file)) as csvfile4:
-                print("error handler 2")
-                with open('/Users/victor/Dropbox (Perspective Group)/Victor/SURVEYS/DAILY WORK/'+ today_date +'CAPITALpython.csv', "a") as output:
-                    print("error handler 3")
-                    reader = csv.DictReader(csvfile4, quotechar='"', delimiter=",", quoting=csv.QUOTE_ALL, skipinitialspace=True)
+            # all data in HashString column replaced with hashed version of data
+            r['HashString'] = hashlib.md5((r['HashString']).encode('utf-8')).hexdigest()
+            # all data in HexString column replaced with ascii hex version of data
+            r['HexString'] = r['HexString'].encode().hex()
 
-                    writer = csv.DictWriter(output, reader.fieldnames)
+            writer.writerow(r)
+            
+            
+            
 
-                    for i, r in enumerate(reader):
+          
+        print("Success - Capital done")
+
+time.sleep(2)
+os.remove('/Users/victor/PYTHON-SURVEY-DOWNLOADS/Capital_Survey_Invites_-_New_Survey.csv')
+
+
+
+#Costa Block
+with open('/Users/victor/PYTHON-SURVEY-DOWNLOADS/Survey_Invite_File_-_Costa_Sur_ALL.csv') as csvfile3:
+    # with open('/Users/victor/12-06-20CAPITAL.csv', "r+") as output:
+    with open('/Users/victor/Dropbox (Perspective Group)/Victor/SURVEYS/DAILY WORK/'+ today_date +'COSTApython.csv', "a") as output:
+        reader = csv.DictReader(csvfile3, quotechar='"', delimiter=",", quoting=csv.QUOTE_ALL, skipinitialspace=True)
+        writer = csv.DictWriter(output, reader.fieldnames)
+        for i, r in enumerate(reader):
+            # writing csv headers
+            if i == 0:
+                output.write(','.join(r) + '\n')
+
+            # all data in HashString column replaced with hashed version of data
+            r['HashString'] = hashlib.md5((r['HashString']).encode('utf-8')).hexdigest()
+            # all data in HexString column replaced with ascii hex version of data
+            r['HexString'] = r['HexString'].encode().hex()
+
+            writer.writerow(r)
+            
+            
+            
+
+          
+        print("Success - Costa done")
+
+time.sleep(2)
+os.remove('/Users/victor/PYTHON-SURVEY-DOWNLOADS/Survey_Invite_File_-_Costa_Sur_ALL.csv')
+
+
+#Galleon Block
+with open('/Users/victor/PYTHON-SURVEY-DOWNLOADS/Galleon_-_Invites.csv') as csvfile3:
+    # with open('/Users/victor/12-06-20CAPITAL.csv', "r+") as output:
+    with open('/Users/victor/Dropbox (Perspective Group)/Victor/SURVEYS/DAILY WORK/'+ today_date +'GALLEONpython.csv', "a") as output:
+        reader = csv.DictReader(csvfile3, quotechar='"', delimiter=",", quoting=csv.QUOTE_ALL, skipinitialspace=True)
+        writer = csv.DictWriter(output, reader.fieldnames)
+        for i, r in enumerate(reader):
+            # writing csv headers
+            if i == 0:
+                output.write(','.join(r) + '\n')
+
+            # all data in HashString column replaced with hashed version of data
+            r['HashString'] = hashlib.md5((r['HashString']).encode('utf-8')).hexdigest()
+            # all data in HexString column replaced with ascii hex version of data
+            r['HexString'] = r['HexString'].encode().hex()
+
+            writer.writerow(r)
+            
+            
+            
+
+          
+        print("Success - Galleon done")
+
+time.sleep(2)
+os.remove('/Users/victor/PYTHON-SURVEY-DOWNLOADS/Galleon_-_Invites.csv')
+
+
+#King's Block
+with open('/Users/victor/PYTHON-SURVEY-DOWNLOADS/Survey_Invite_-_Kings_Creek.csv') as csvfile4:
+    # with open('/Users/victor/12-06-20CAPITAL.csv', "r+") as output:
+    with open('/Users/victor/Dropbox (Perspective Group)/Victor/SURVEYS/DAILY WORK/'+ today_date +'KINGSpython.csv', "a") as output:
+        reader = csv.DictReader(csvfile4, quotechar='"', delimiter=",", quoting=csv.QUOTE_ALL, skipinitialspace=True)
+        writer = csv.DictWriter(output, reader.fieldnames)
+        for i, r in enumerate(reader):
+            # writing csv headers
+            if i == 0:
+                output.write(','.join(r) + '\n')
+
+            # all data in HashString column replaced with hashed version of data
+            r['HashString'] = hashlib.md5((r['HashString']).encode('utf-8')).hexdigest()
+            # all data in HexString column replaced with ascii hex version of data
+            r['HexString'] = r['HexString'].encode().hex()
+
+            writer.writerow(r)
+            
+            
+            
+
+          
+        print("Success - King's Creek done")
+
+time.sleep(2)
+os.remove('/Users/victor/PYTHON-SURVEY-DOWNLOADS/Survey_Invite_-_Kings_Creek.csv')
+
+
+
+
+
+
+
+
+
+# print('-------------------------Ideal Loop Version---------------------------------')
+# now = datetime.now()
+# # today_date = now.strftime("%h:%M:S-%m-%d-%y")
+# today_date = now.strftime('%m-%d-%Y-%H:%M:%S.%f')
+# print("The date is: ", today_date)
+
+
+# # Capital Block
+# path = os.getcwd()
+# print(path)
+# # os.chdir(os.path.dirname('/Users/victor/PYTHON-SURVEY-DOWNLOADS/'))
+# directory = '/Users/victor/PYTHON-SURVEY-DOWNLOADS/'
+
+# # for root,dirs,files in os.walk(path): 
+# for file in os.listdir(directory):
+#     # for file in files:
+#         # print("verified files are only csv...")
+#         if file.endswith('.csv'):
+#             print(os.path.join(directory, file))
+#             # f=open(file, 'r')
+#             # with open('/Users/victor/PYTHON-SURVEY-DOWNLOADS/Capital_Survey_Invites_-_New_Survey.csv') as csvfile4:
+#             with open(os.path.join(directory, file)) as csvfile4:
+#                 print("Opening input file...")
+#                 with open('/Users/victor/Dropbox (Perspective Group)/Victor/SURVEYS/DAILY WORK/'+ today_date +'-CAPITALpython.csv', "a+") as output:
+#                     print("Opening export file...")
+#                     reader = csv.DictReader(csvfile4, quotechar='"', delimiter=",", quoting=csv.QUOTE_ALL, skipinitialspace=True)
+
+#                     writer = csv.DictWriter(output, reader.fieldnames)
+
+#                     for i, r in enumerate(reader):
                     
-                        # writing csv headers
+#                         # writing csv headers
 
-                        if i == 0:
+#                         if i == 0:
                         
-                            output.write(','.join(r) + '\n')
+#                             output.write(','.join(r) + '\n')
 
 
 
-                        # all data in HashString column replaced with hashed version of data
+#                         # all data in HashString column replaced with hashed version of data
 
-                        r['HashString'] = hashlib.md5((r['HashString']).encode('utf-8')).hexdigest()
+#                         r['HashString'] = hashlib.md5((r['HashString']).encode('utf-8')).hexdigest()
 
-                        # all data in HexString column replaced with ascii hex version of data
+#                         # all data in HexString column replaced with ascii hex version of data
 
-                        r['HexString'] = r['HexString'].encode().hex()
+#                         r['HexString'] = r['HexString'].encode().hex()
 
-                        writer.writerow(r)
+#                         writer.writerow(r)
+
                         
 
 
-                    print("Success - done")
+#                     print("Success - done")
 
 
 
-            time.sleep(5)
+#             time.sleep(5)
 
 
     
-    # os.remove('/Users/victor/PYTHON-SURVEY-DOWNLOADS/Capital_Survey_Invites_-_New_Survey.csv')
-# Costa Block
-# Galleon Block
-# King's Creek Block
+#     # os.remove('/Users/victor/PYTHON-SURVEY-DOWNLOADS/Capital_Survey_Invites_-_New_Survey.csv')
+
