0f56cb21b7b1ec36ffdbac8055788d0e4474bf9e
Victor <victor@Hans-MacBook-Air.local>
2020-12-08 16:52:20 -0800

    Eliminate dup emails coming next

--- excelWork.py
+++ excelWork.py
@@ -6,24 +6,20 @@ from io import StringIO
 from datetime import datetime
 import os
 import time
+import pandas as pd
 
 print('')
 print('-------------------------Converting HashString column to MD5 Hash & HexString column to ascii hex---------------------------------')
 print('')
 
 now = datetime.now()
-today_date = now.strftime("%M-%m-%d-%Y")
-print("Good Morning! Today is: ", today_date)
-
-
-
-
+today_date = now.strftime("%m-%d-%Y")
+print("Good Morning! Today is:", today_date)
 
 
 # Capital Block
 with open('/Users/victor/PYTHON-SURVEY-DOWNLOADS/Capital_Survey_Invites_-_New_Survey.csv') as csvfile:
-    # with open('/Users/victor/12-06-20CAPITAL.csv', "r+") as output:
-    with open('/Users/victor/Dropbox (Perspective Group)/Victor/SURVEYS/DAILY WORK/'+ today_date +'CAPITALpython.csv', "a") as output:
+    with open('/Users/victor/Dropbox (Perspective Group)/Victor/SURVEYS/DAILY WORK/'+ today_date +'CAPITAL.csv', "a") as output:
         reader = csv.DictReader(csvfile, quotechar='"', delimiter=",", quoting=csv.QUOTE_ALL, skipinitialspace=True)
         writer = csv.DictWriter(output, reader.fieldnames)
         for i, r in enumerate(reader):
@@ -36,15 +32,10 @@ with open('/Users/victor/PYTHON-SURVEY-DOWNLOADS/Capital_Survey_Invites_-_New_Su
             # all data in HexString column replaced with ascii hex version of data
             r['HexString'] = r['HexString'].encode().hex()
 
-            writer.writerow(r)
-            
-            
-            
-
-          
+            writer.writerow(r)   
         print("Success - Capital done")
 
-time.sleep(2)
+time.sleep(5)
 os.remove('/Users/victor/PYTHON-SURVEY-DOWNLOADS/Capital_Survey_Invites_-_New_Survey.csv')
 
 
@@ -52,7 +43,7 @@ os.remove('/Users/victor/PYTHON-SURVEY-DOWNLOADS/Capital_Survey_Invites_-_New_Su
 #Costa Block
 with open('/Users/victor/PYTHON-SURVEY-DOWNLOADS/Survey_Invite_File_-_Costa_Sur_ALL.csv') as csvfile3:
     # with open('/Users/victor/12-06-20CAPITAL.csv', "r+") as output:
-    with open('/Users/victor/Dropbox (Perspective Group)/Victor/SURVEYS/DAILY WORK/'+ today_date +'COSTApython.csv', "a") as output:
+    with open('/Users/victor/Dropbox (Perspective Group)/Victor/SURVEYS/DAILY WORK/'+ today_date +'COSTA.csv', "a") as output:
         reader = csv.DictReader(csvfile3, quotechar='"', delimiter=",", quoting=csv.QUOTE_ALL, skipinitialspace=True)
         writer = csv.DictWriter(output, reader.fieldnames)
         for i, r in enumerate(reader):
@@ -73,14 +64,14 @@ with open('/Users/victor/PYTHON-SURVEY-DOWNLOADS/Survey_Invite_File_-_Costa_Sur_
           
         print("Success - Costa done")
 
-time.sleep(2)
+time.sleep(5)
 os.remove('/Users/victor/PYTHON-SURVEY-DOWNLOADS/Survey_Invite_File_-_Costa_Sur_ALL.csv')
 
 
 #Galleon Block
 with open('/Users/victor/PYTHON-SURVEY-DOWNLOADS/Galleon_-_Invites.csv') as csvfile3:
     # with open('/Users/victor/12-06-20CAPITAL.csv', "r+") as output:
-    with open('/Users/victor/Dropbox (Perspective Group)/Victor/SURVEYS/DAILY WORK/'+ today_date +'GALLEONpython.csv', "a") as output:
+    with open('/Users/victor/Dropbox (Perspective Group)/Victor/SURVEYS/DAILY WORK/'+ today_date +'GALLEON.csv', "a") as output:
         reader = csv.DictReader(csvfile3, quotechar='"', delimiter=",", quoting=csv.QUOTE_ALL, skipinitialspace=True)
         writer = csv.DictWriter(output, reader.fieldnames)
         for i, r in enumerate(reader):
@@ -101,14 +92,14 @@ with open('/Users/victor/PYTHON-SURVEY-DOWNLOADS/Galleon_-_Invites.csv') as csvf
           
         print("Success - Galleon done")
 
-time.sleep(2)
+time.sleep(5)
 os.remove('/Users/victor/PYTHON-SURVEY-DOWNLOADS/Galleon_-_Invites.csv')
 
 
 #King's Block
 with open('/Users/victor/PYTHON-SURVEY-DOWNLOADS/Survey_Invite_-_Kings_Creek.csv') as csvfile4:
     # with open('/Users/victor/12-06-20CAPITAL.csv', "r+") as output:
-    with open('/Users/victor/Dropbox (Perspective Group)/Victor/SURVEYS/DAILY WORK/'+ today_date +'KINGSpython.csv', "a") as output:
+    with open('/Users/victor/Dropbox (Perspective Group)/Victor/SURVEYS/DAILY WORK/'+ today_date +'KINGS.csv', "a") as output:
         reader = csv.DictReader(csvfile4, quotechar='"', delimiter=",", quoting=csv.QUOTE_ALL, skipinitialspace=True)
         writer = csv.DictWriter(output, reader.fieldnames)
         for i, r in enumerate(reader):
@@ -129,7 +120,7 @@ with open('/Users/victor/PYTHON-SURVEY-DOWNLOADS/Survey_Invite_-_Kings_Creek.csv
           
         print("Success - King's Creek done")
 
-time.sleep(2)
+time.sleep(5)
 os.remove('/Users/victor/PYTHON-SURVEY-DOWNLOADS/Survey_Invite_-_Kings_Creek.csv')
 
 
