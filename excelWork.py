9c2c65a531eceb97a825625e05f9f4a8189805f9
Victor <victor@Hans-MacBook-Air.local>
2020-12-09 10:51:15 -0800

    all genereated files will get new columns now. Next is to eliminate duplicates

--- excelWork.py
+++ excelWork.py
@@ -22,25 +22,24 @@ if os.path.isfile('/Users/victor/PYTHON-SURVEY-DOWNLOADS/Capital_Survey_Invites_
     with open('/Users/victor/PYTHON-SURVEY-DOWNLOADS/Capital_Survey_Invites_-_New_Survey.csv') as csvfile: 
         with open('/Users/victor/Dropbox (Perspective Group)/Victor/SURVEYS/DAILY WORK/'+ today_date +'CAPITAL.csv', "a") as output:
             reader = csv.DictReader(csvfile, quotechar='"', delimiter=",", quoting=csv.QUOTE_ALL, skipinitialspace=True)
-            writer = csv.DictWriter(output, reader.fieldnames)
-            for i, r in enumerate(reader):
-                # writing csv headers
-                if i == 0:
-                    output.write(','.join(r) + '\n')
-
+            fieldnames = reader.fieldnames + ['TA1', 'TA2']
+            writer = csv.DictWriter(output, fieldnames)
+            writer.writeheader()
+            # fieldnames = reader.fieldnames + ['NewColum1'] 
+            for i, r in enumerate(reader):        
                 # all data in HashString column replaced with hashed version of data
-                r['HashString'] = hashlib.md5((r['HashString']).encode('utf-8')).hexdigest()
+                r['TA1'] = hashlib.md5((r['HashString']).encode('utf-8')).hexdigest()
                 # all data in HexString column replaced with ascii hex version of data
-                r['HexString'] = r['HexString'].encode().hex()
+                r['TA2'] = r['HexString'].encode().hex()
 
                 writer.writerow(r)   
             print("Success - Capital done")
 else:
-    print("Nothing for Capital today")
+    print("Nothing for Capital today :(")
 
 print("Loading next...")
 
-time.sleep(.1)
+time.sleep(3)
 # os.remove('/Users/victor/PYTHON-SURVEY-DOWNLOADS/Capital_Survey_Invites_-_New_Survey.csv')
 
 
@@ -56,11 +55,7 @@ if os.path.isfile('/Users/victor/PYTHON-SURVEY-DOWNLOADS/Survey_Invite_File_-_Co
             writer = csv.DictWriter(output, fieldnames)
             writer.writeheader()
             # fieldnames = reader.fieldnames + ['NewColum1'] 
-            for i, r in enumerate(reader):
-                # writing csv headers
-                # if i == 0:
-                #     output.write(','.join(r) + '\n')
-        
+            for i, r in enumerate(reader):        
                 # all data in HashString column replaced with hashed version of data
                 r['TA1'] = hashlib.md5((r['HashString']).encode('utf-8')).hexdigest()
                 # all data in HexString column replaced with ascii hex version of data
@@ -72,7 +67,7 @@ if os.path.isfile('/Users/victor/PYTHON-SURVEY-DOWNLOADS/Survey_Invite_File_-_Co
 
             print("Success - Costa done")
 else:
-    print("Nothing for Costa today")
+    print("Nothing for Costa today :(")
 
 print("Loading next...")
 time.sleep(3)
@@ -85,22 +80,21 @@ if os.path.isfile('/Users/victor/PYTHON-SURVEY-DOWNLOADS/Galleon_-_Invites.csv')
         # with open('/Users/victor/12-06-20CAPITAL.csv', "r+") as output:
         with open('/Users/victor/Dropbox (Perspective Group)/Victor/SURVEYS/DAILY WORK/'+ today_date +'GALLEON.csv', "a") as output:
             reader = csv.DictReader(csvfile3, quotechar='"', delimiter=",", quoting=csv.QUOTE_ALL, skipinitialspace=True)
-            writer = csv.DictWriter(output, reader.fieldnames)
-            for i, r in enumerate(reader):
-                # writing csv headers
-                if i == 0:
-                    output.write(','.join(r) + '\n')
-
+            fieldnames = reader.fieldnames + ['TA1', 'TA2']
+            writer = csv.DictWriter(output, fieldnames)
+            writer.writeheader()
+            # fieldnames = reader.fieldnames + ['NewColum1'] 
+            for i, r in enumerate(reader):        
                 # all data in HashString column replaced with hashed version of data
-                r['HashString'] = hashlib.md5((r['HashString']).encode('utf-8')).hexdigest()
+                r['TA1'] = hashlib.md5((r['HashString']).encode('utf-8')).hexdigest()
                 # all data in HexString column replaced with ascii hex version of data
-                r['HexString'] = r['HexString'].encode().hex()
+                r['TA2'] = r['HexString'].encode().hex()
 
                 writer.writerow(r)
 
             print("Success - Galleon done")
 else:
-    print("Nothing for Galleon today")
+    print("Nothing for Galleon today :(")
 
 print("Loading next...")
 time.sleep(3)
@@ -113,22 +107,21 @@ if os.path.isfile('/Users/victor/PYTHON-SURVEY-DOWNLOADS/Survey_Invite_-_Kings_C
         # with open('/Users/victor/12-06-20CAPITAL.csv', "r+") as output:
         with open('/Users/victor/Dropbox (Perspective Group)/Victor/SURVEYS/DAILY WORK/'+ today_date +'KINGS.csv', "a") as output:
             reader = csv.DictReader(csvfile4, quotechar='"', delimiter=",", quoting=csv.QUOTE_ALL, skipinitialspace=True)
-            writer = csv.DictWriter(output, reader.fieldnames)
-            for i, r in enumerate(reader):
-                # writing csv headers
-                if i == 0:
-                    output.write(','.join(r) + '\n')
-
+            fieldnames = reader.fieldnames + ['TA1', 'TA2']
+            writer = csv.DictWriter(output, fieldnames)
+            writer.writeheader()
+            # fieldnames = reader.fieldnames + ['NewColum1'] 
+            for i, r in enumerate(reader):        
                 # all data in HashString column replaced with hashed version of data
-                r['HashString'] = hashlib.md5((r['HashString']).encode('utf-8')).hexdigest()
+                r['TA1'] = hashlib.md5((r['HashString']).encode('utf-8')).hexdigest()
                 # all data in HexString column replaced with ascii hex version of data
-                r['HexString'] = r['HexString'].encode().hex()
+                r['TA2'] = r['HexString'].encode().hex()
 
                 writer.writerow(r)
 
             print("Success - King's Creek done")
 else: 
-    print("Nothing for King's Creek today")
+    print("Nothing for King's Creek today :(")
 
 print("All done")
 time.sleep(3)
