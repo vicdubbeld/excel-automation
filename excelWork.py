3405c5a586eaf04f00a7f23f37c0ec4b6358f57d
Victor <victor@Hans-MacBook-Air.local>
2020-12-14 12:24:09 -0800

    row counter deprecated

--- excelWork.py
+++ excelWork.py
@@ -14,7 +14,8 @@ import sys
 
 now = datetime.now()
 today_date = now.strftime("%m-%d-%Y")
-today_date2 = now.strftime("%m-%d-%Y" + " %H:%M:%S")
+today_date2 = now.strftime("%A, %m-%d-%Y" + " %H:%M:%S")
+
 print('')
 print('')
 print('-------------------------Welcome to the master automation machine---------------------------------')
@@ -50,7 +51,7 @@ kingsta2 = '/Users/victor/PYTHON-SURVEY-DOWNLOADS/TA_Survey_Invite_Report (clean
 kingsta3 = '/Users/victor/Dropbox (Perspective Group)/Victor/SURVEYS/DAILY WORK/'+ today_date +'KINGSTA.csv'
 
 time.sleep(1)
-print("Loading Capital")
+print("Loading Capital\n")
 # Capital Block
 if os.path.isfile(capital) == True:
     # Read input file 'csvfile'
@@ -60,9 +61,6 @@ if os.path.isfile(capital) == True:
     with open(capital2) as csvfile: 
         with open(capital3, "a") as output:
             reader = csv.DictReader(csvfile, quotechar='"', delimiter=",", quoting=csv.QUOTE_ALL, skipinitialspace=True)
-            # Counts # of records
-            row_count = len(list(reader))
-            print("The count is: ", row_count)
             # Define new columns for hashed/hexed data in output file
             fieldnames = reader.fieldnames + ['TA1', 'TA2']
             writer = csv.DictWriter(output, fieldnames)
@@ -81,14 +79,14 @@ if os.path.isfile(capital) == True:
             time.sleep(1)
             print("Success - Capital done")
             time.sleep(1)
-            print("Removing input files...")
+            print("Removing input files...\n")
             # os.remove(capital)
             # os.remove(capital2)
 else:
     time.sleep(1)
     print("Nothing for Capital today :(")
 time.sleep(1)
-print("Loading Capital TA...")
+print("Loading Capital TA...\n")
 
 
 # Capital TA Block
@@ -100,9 +98,6 @@ if os.path.isfile(capitalta) == True:
     with open(capitalta2) as csvfile: 
         with open(capitalta3, "a") as output:
             reader = csv.DictReader(csvfile, quotechar='"', delimiter=",", quoting=csv.QUOTE_ALL, skipinitialspace=True)
-             # Counts # of records
-            row_count = len(list(reader))
-            print("The count is: ", row_count)
             # Define new columns for hashed/hexed data in output file
             fieldnames = reader.fieldnames + ['TA1', 'TA2']
             writer = csv.DictWriter(output, fieldnames)
@@ -121,15 +116,15 @@ if os.path.isfile(capitalta) == True:
             time.sleep(1)
             print("Success - Capital TA done")
             time.sleep(1)
-            print("Removing input files")
+            print("Removing input files\n")
             # os.remove(capitalta)
             # os.remove(capitalta2)
    
 else:
     time.sleep(1)
-    print("Nothing for Capital TA today :(")
+    print("Nothing for Capital TA today :(\n")
 time.sleep(1)
-print("Loading Costa...")
+print("Loading Costa...\n")
 
 
 #Costa Block
@@ -140,9 +135,7 @@ if os.path.isfile(costa) == True:
     with open(costa2) as csvfile3:
         with open(costa3, "a") as output:
             reader = csv.DictReader(csvfile3, quotechar='"', delimiter=",", quoting=csv.QUOTE_ALL, skipinitialspace=True)
-             # Counts # of records
-            row_count = len(list(reader))
-            print("The count is: ", row_count)
+            
             # Add new columns for hashed/hexed data
             fieldnames = reader.fieldnames + ['TA1', 'TA2']
             writer = csv.DictWriter(output, fieldnames)
@@ -165,14 +158,15 @@ if os.path.isfile(costa) == True:
             time.sleep(1)    
             print("Success - Costa done")
             time.sleep(1)
+            print("Removing input files\n")
             # deletes input file
             # os.remove(costa)
             # os.remove(costa2)
 else:
     time.sleep(1)
-    print("Nothing for Costa today :(")
+    print("Nothing for Costa today :(\n")
 time.sleep(1)
-print("Loading Costa TA...")
+print("Loading Costa TA...\n")
 
 
 #Costa TA Block
@@ -183,9 +177,6 @@ if os.path.isfile(costata) == True:
     with open(costata2) as csvfile3:
         with open(costata3, "a") as output:
             reader = csv.DictReader(csvfile3, quotechar='"', delimiter=",", quoting=csv.QUOTE_ALL, skipinitialspace=True)
-             # Counts # of records
-            row_count = len(list(reader))
-            print("The count is: ", row_count)
             # Add new columns for hashed/hexed data
             fieldnames = reader.fieldnames + ['TA1', 'TA2']
             writer = csv.DictWriter(output, fieldnames)
@@ -207,14 +198,15 @@ if os.path.isfile(costata) == True:
             time.sleep(1)    
             print("Success - Costa TA done")
             time.sleep(1)
+            print("Removing input files\n")
             # deletes input file
             # os.remove(costata)
             # os.remove(costata2)
 else:
     time.sleep(1)
-    print("Nothing for Costa TA today because it's not Wednesday :(")
+    print("Nothing for Costa TA today because it's not Wednesday :(\n")
 time.sleep(1)
-print("Loading Galleon...")
+print("Loading Galleon...\n")
 
 
 #Galleon Block
@@ -225,9 +217,6 @@ if os.path.isfile(galleon) == True:
     with open(galleon2) as csvfile3:
         with open(galleon3, "a") as output:
             reader = csv.DictReader(csvfile3, quotechar='"', delimiter=",", quoting=csv.QUOTE_ALL, skipinitialspace=True)
-             # Counts # of records
-            row_count = len(list(reader))
-            print("The count is: ", row_count)
             # Add new columns for hashed/hexed data
             fieldnames = reader.fieldnames + ['TA1', 'TA2']
             writer = csv.DictWriter(output, fieldnames)
@@ -242,15 +231,16 @@ if os.path.isfile(galleon) == True:
                 writer.writerow(r)
             time.sleep(1)
             print("Success - Galleon done")
-            time.sleep(1)   
+            time.sleep(1)  
+            print("Removing input files\n") 
             # deletes input file
             # os.remove(galleon)
             # os.remove(galleon2)
 else:
     time.sleep(1)
-    print("Nothing for Galleon today :(")
+    print("Nothing for Galleon today :(\n")
 time.sleep(1)
-print("Loading Kings...")
+print("Loading Kings...\n")
 
 
 #King's Block
@@ -260,9 +250,6 @@ if os.path.isfile(kings) == True:
     with open(kings2) as csvfile4:
         with open(kings3, "a") as output:
             reader = csv.DictReader(csvfile4, quotechar='"', delimiter=",", quoting=csv.QUOTE_ALL, skipinitialspace=True)
-             # Counts # of records
-            row_count = len(list(reader))
-            print("The count is: ", row_count)
             # Add new columns for hashed/hexed data
             fieldnames = reader.fieldnames + ['TA1', 'TA2']
             writer = csv.DictWriter(output, fieldnames)
@@ -278,14 +265,15 @@ if os.path.isfile(kings) == True:
             time.sleep(1)
             print("Success - King's Creek done")
             time.sleep(1)
+            print("Removing input files\n") 
             # deletes input file
             # os.remove(kings)
             # os.remove(kings2)
 else:
     time.sleep(1) 
-    print("Nothing for King's Creek today :(")
+    print("Nothing for King's Creek today :(\n")
 time.sleep(1)
-print("Loading Kings TA...")
+print("Loading Kings TA...\n")
 
 #King's TA Block
 if os.path.isfile(kingsta) == True:
@@ -294,9 +282,6 @@ if os.path.isfile(kingsta) == True:
     with open(kingsta2) as csvfile4:
         with open(kingsta3, "a") as output:
             reader = csv.DictReader(csvfile4, quotechar='"', delimiter=",", quoting=csv.QUOTE_ALL, skipinitialspace=True)
-             # Counts # of records
-            row_count = len(list(reader))
-            print("The count is: ", row_count)
             # Add new columns for hashed/hexed data
             fieldnames = reader.fieldnames + ['TA1', 'TA2']
             writer = csv.DictWriter(output, fieldnames)
@@ -309,19 +294,19 @@ if os.path.isfile(kingsta) == True:
                 r['TA2'] = r['HexString'].encode().hex()
 
                 writer.writerow(r)
-            time.sleep(2)
+            time.sleep(1)
             print("Success - King's Creek TA done")
-            time.sleep(2)
+            time.sleep(1)
+            print("Removing input files\n") 
             # deletes input file
             # os.remove(kingsta)
             # os.remove(kingsta2)
 else:
     time.sleep(1) 
-    print("Nothing for King's Creek TA today because it's not Wednesday :(")
+    print("Nothing for King's Creek TA today because it's not Wednesday :(\n")
 time.sleep(1)
 
 
-print('')
 print('')
 print("All done! Now go make yourself a nice cup of coffee. You deserve it after all the hard work.")
 print('')
