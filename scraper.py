ce9c38214e61b2010ba6725fdd59f501ff8f3883
Victor <victor@Hans-MacBook-Air.local>
2020-12-16 12:35:07 -0800

    full contact upload cycle complete

--- scraper.py
+++ scraper.py
@@ -1,10 +1,15 @@
 from selenium import webdriver
 import time
+from datetime import datetime
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from webdriver_manager.chrome import ChromeDriverManager
+import os
 
 # Web Scraper to upload to contact manager
+
+now = datetime.now()
+today_date = now.strftime("%m-%d-%Y")
 
 print('')
 print("Let the webscraping begin! \n")
@@ -19,22 +24,35 @@ driver.get(url)
 driver.find_element_by_xpath('//*[@id="login"]').send_keys("email")
 driver.find_element_by_xpath('//*[@id="password"]').send_keys("password")
 driver.find_element_by_id("loginButton").click()
-print('success')
-
+print('success\n')
+time.sleep(3)
 # contacts tab
 driver.find_element_by_name('LABEL_CONTACTS').click()
 # keys("shift") x 2
 actions = ActionChains(driver)
+print('navigating to contacts...\n')
 
 # capital
-n = 18
+# selects CAPITAL TEST
+n = 19
 actions.send_keys(Keys. TAB * n)
 actions.send_keys(Keys. ENTER)
 actions.perform()
-
-time.sleep(5)
+print('Capital Started...\n')
+time.sleep(3)
 
 # import
+time.sleep(2)
 driver.find_element_by_xpath('//*[@id="divImport"]').click()
-# driver.find_element_by_id('divImport').click()
-
+# import page set up new import
+time.sleep(2)
+driver.find_element_by_xpath('//*[@id="mainIncludeContainer"]/table/tbody/tr/td/div[1]/button').click()
+print('set up new import')
+# choose file
+time.sleep(2)
+driver.find_element_by_name('localLocation').send_keys(os.path.join(os.getcwd(), '/Users/victor/Dropbox (Perspective Group)/Victor/SURVEYS/DAILY WORK/'+ today_date +'CAPITAL.csv'))
+print('choosing file...\n')
+driver.find_element_by_xpath('/html/body/div[1]/div/div[5]/button[2]').click()
+time.sleep(2)
+driver.find_element_by_xpath('/html/body/div[1]/div/div[5]/button[2]').click()
+driver.find_element_by_xpath('//*[@id="theForm"]/div[1]/button[2]').click()
