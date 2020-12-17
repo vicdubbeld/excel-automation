5a8ab67ac88149a931be1dfdce75ac7db5f301f7
Victor <victor@Hans-MacBook-Air.local>
2020-12-17 08:14:43 -0800

    function cleanup and variable definitions

--- excelWork.py
+++ excelWork.py
@@ -323,3 +323,6 @@ url = 'https://survey.intuitionbrandmarketing.com/Member/ContactView/List.action
 browser = webdriver.Chrome()
 
 browser.find_element_by_xpath('//*[@id=treeViewNode_1_-41512190_1_41512190"]').click()
+
+
+
--- scraper.py
+++ scraper.py
@@ -8,8 +8,17 @@ import os
 
 # Web Scraper to upload to contact manager
 
+# Establishing time 
 now = datetime.now()
 today_date = now.strftime("%m-%d-%Y")
+
+# login credentials 
+user = 'username'
+password = 'password'
+
+# Defining paths to files that will be uploaded in contact manager
+capital_test = '/Users/victor/Dropbox (Perspective Group)/Victor/SURVEYS/DAILY WORK/'+ today_date +'CAPITAL.csv'
+
 
 print('')
 print("Let the webscraping begin! \n")
@@ -20,17 +29,20 @@ driver = webdriver.Chrome(ChromeDriverManager().install())
 # auto log into site
 
 # login 
-driver.get(url)
-driver.find_element_by_xpath('//*[@id="login"]').send_keys("email")
-driver.find_element_by_xpath('//*[@id="password"]').send_keys("password")
-driver.find_element_by_id("loginButton").click()
-print('success\n')
-time.sleep(3)
-# contacts tab
-driver.find_element_by_name('LABEL_CONTACTS').click()
-# keys("shift") x 2
-actions = ActionChains(driver)
-print('navigating to contacts...\n')
+def login():
+    driver.get(url)
+    driver.find_element_by_xpath('//*[@id="login"]').send_keys(user)
+    driver.find_element_by_xpath('//*[@id="password"]').send_keys(password)
+    driver.find_element_by_id("loginButton").click()
+    print('success\n')
+    time.sleep(3)
+    # contacts tab
+    driver.find_element_by_name('LABEL_CONTACTS').click()
+    # keys("shift") x 2
+    actions = ActionChains(driver)
+    print('navigating to contacts...\n')
+
+login()
 
 # capital
 # selects CAPITAL TEST
@@ -38,19 +50,19 @@ n = 19
 actions.send_keys(Keys. TAB * n)
 actions.send_keys(Keys. ENTER)
 actions.perform()
-print('Capital Started...\n')
+print('Capital Test Selected...\n')
 time.sleep(3)
 
-# import
+# import button 
 time.sleep(2)
 driver.find_element_by_xpath('//*[@id="divImport"]').click()
-# import page set up new import
+# import page -> set up new import
 time.sleep(2)
 driver.find_element_by_xpath('//*[@id="mainIncludeContainer"]/table/tbody/tr/td/div[1]/button').click()
-print('set up new import')
+print('setting up new import...\n')
 # choose file
 time.sleep(2)
-driver.find_element_by_name('localLocation').send_keys(os.path.join(os.getcwd(), '/Users/victor/Dropbox (Perspective Group)/Victor/SURVEYS/DAILY WORK/'+ today_date +'CAPITAL.csv'))
+driver.find_element_by_name('localLocation').send_keys(os.path.join(os.getcwd(), capital_test))
 print('choosing file...\n')
 driver.find_element_by_xpath('/html/body/div[1]/div/div[5]/button[2]').click()
 time.sleep(2)
